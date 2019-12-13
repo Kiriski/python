b9f338c7ae0c67a6bb37ab6c4b7ab16ccab3ff08
Kiriski <52634320+Kiriski@users.noreply.github.com>
2019-12-13 16:04:25 +0100

    commit de la class

---
+++ pycharm/temp.py
@@ -0,0 +1,9 @@
+
+
+chaine = "Bonjour les ZER0S"
+for lettre in chaine:
+    print(lettre)
+    if lettre in "AEIOUYaeiouy": # lettre est une voyelle
+        print("-> " + lettre)
+    else: # lettre est une consonne... ou plus exactement, lettre n'est pas une voyelle
+        print("-> *")
